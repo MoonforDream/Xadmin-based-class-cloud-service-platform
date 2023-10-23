c220ea8f86a58ded005ca30164128b1ce9a4b7fe
Moon <3052573970@qq.com>
2023-10-23 16:36:58 +0800

    Update settings.py

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -160,8 +160,8 @@ PAGINATION_SETTINGS = {
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.qq.com' #腾讯QQ邮箱SMTP服务器地址
 EMAIL_PORT=25 #SMTP服务的端口号
-EMAIL_HOST_USER='3052573970@qq.com' #发送邮件的QQ邮箱
-EMAIL_HOST_PASSWORD='zzypvnpkjonjdcji' #在QQ邮箱->设置->账户->"POP3/IMAP...服务"里得到的在第三方登录QQ邮箱授权码
+EMAIL_HOST_USER='你的邮箱' #发送邮件的QQ邮箱
+EMAIL_HOST_PASSWORD='你的邮箱授权码' #在QQ邮箱->设置->账户->"POP3/IMAP...服务"里得到的在第三方登录QQ邮箱授权码
 EMAIL_USE_TLS=False #与SMTP服务器通信时，是否启动TLS链接(安全链接)默认False
 
 
